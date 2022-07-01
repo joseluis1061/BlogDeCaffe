29e5ef94f8cd33fffcdaa508408a3be2f233c034
jose <joseluis836@hotmail.com>
2022-07-01 12:10:12 -0500

    Se agrego un archivo de configuración para hacer deploy en netlify

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
   //mode: production,
 
