b7cada15bf225e8872ad6c622752e074feeb9921
jose <joseluis836@hotmail.com>
2022-07-01 10:04:37 -0500

    Se creo el archivo de configuracion de webpack. Se definio el punto de entrada y salida para el proyecto

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  //mode: production,
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+
+  resolve: {
+    extensions:['.js']
+  }
+
+}
